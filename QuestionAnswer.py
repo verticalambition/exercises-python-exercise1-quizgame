dd30195bdd61bea2a424ff6012b0750884134f2c
Jason <jasonelish@gmail.com>
2022-08-01 20:25:49 -0600

    Initial commit

---
+++ QuestionAnswer.py
@@ -0,0 +1,4 @@
+class QuestionAnswer:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
---
+++ main.py
@@ -0,0 +1,9 @@
+from QuestionAnswer import QuestionAnswer
+
+
+def main():
+    print("Just getting started")
+
+
+if __name__ == "__main__":
+    main()
